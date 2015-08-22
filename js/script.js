ee919ad30aa08edc57ed34ba04030a8d53f56505
Gleb <git@gleb.nl>
2015-08-23 01:15:08 +0200

    Refactored dimension calculation again.

--- js/script.js
+++ js/script.js
@@ -43,94 +43,83 @@ var tab = {
       tab.setLinks();
     });
 
+    $('#btn-plus').bind('click', function() {
+      var i = ( tab.arr.indexOf(tab.d) > -1 ) ? tab.arr.indexOf(tab.d) : tab.arr.indexOf(tab.closest(tab.d, tab.arr));
+      if ( i === tab.arr.length-1 ) return;
+      tab.d = tab.arr[i+1];
+      tab.setLinks();
+      chrome.storage.sync.set({'dimensions': tab.d});
+    });
+
+    $('#btn-minus').bind('click', function() {
+      var i = ( tab.arr.indexOf(tab.d) > -1 ) ? tab.arr.indexOf(tab.d) : tab.arr.indexOf(tab.closest(tab.d, tab.arr));
+      if ( i === 0 ) return;
+      tab.d = tab.arr[i-1];
+      tab.setLinks();
+      chrome.storage.sync.set({'dimensions': tab.d});
+    });
+
     // set click listener for the edit button
     $('#edit').click(function() {
-        window.editMode = true;
-    });
-
-    var rowHeight = 100;
-    var colWidth = 100;
-    var prev = {r: 100, c: 100};
-    var r, c;
-    while ( rowHeight <= 1500 ) {
-      r = c = window.innerWidth / Math.ceil(window.innerWidth / colWidth);
-      if ( !(prev.r === r && prev.c === c) ) tab.arr.push({r:r,c:c});
-      rowHeight = colWidth = rowHeight + 1;
-      prev = {r: r, c: c};
-    }
-
-    var i = 0;
-    var timeoutPlus = 0;
-    $('#btn-plus').bind('mousedown', function() {
-      timeoutPlus = setInterval(function () {
-        tab.rowHeight = tab.arr[i].r;
-        tab.colWidth = tab.arr[i].c;
-        i++;
-        if ( i === tab.arr.length ) i--;
-        tab.setLinks();
-        chrome.storage.sync.set({'dimensions': {rowHeight: tab.rowHeight, colWidth: tab.colWidth}});
-      }, 150);
-    }).bind('mouseup mouseleave', function() {
-      clearTimeout(timeoutPlus);
-    });
-
-    var timeoutMinus = 0;
-    $('#btn-minus').bind('mousedown', function() {
-      timeoutMinus = setInterval(function () {
-        tab.rowHeight = tab.arr[i].r;
-        tab.colWidth = tab.arr[i].c;
-        i--;
-        if ( i < 0 ) i = 0;
-        tab.setLinks();
-        chrome.storage.sync.set({'dimensions': {rowHeight: tab.rowHeight, colWidth: tab.colWidth}});
-      }, 150);
-    }).bind('mouseup mouseleave', function() {
-      clearTimeout(timeoutMinus);
+        tab.editMode = true;
     });
 
     // set submit listener for the link edit form
     $('#link_edit_submit').click(function() {
-        window.links[$('#link_id').val()] = {
-            id: $('#link_id').val(),
-            title: $('#link_title').val() || "",
-            url: $('#link_url').val() || "",
-            image: window.imageData
-        }
-        chrome.storage.local.set({'links': window.links},function() {});
-        window.location.reload();
+      window.links[$('#link_id').val()] = {
+        id: $('#link_id').val(),
+        title: $('#link_title').val() || "",
+        url: $('#link_url').val() || "",
+        image: window.imageData
+      }
+      chrome.storage.local.set({'links': window.links},function() {});
+      window.location.reload();
     });
 
     // set a change event listener on the image file field
     $('#link_image').change(function(e){
-        file = e.target.files[0];
-        var reader = new FileReader();
-        reader.onload = function (event) {
-            window.imageData = event.target.result;
-        };
-        reader.readAsDataURL(file);
+      file = e.target.files[0];
+      var reader = new FileReader();
+      reader.onload = function (event) {
+          window.imageData = event.target.result;
+      };
+      reader.readAsDataURL(file);
     });
 
     // set click listener on the link delete button
     $('#delete_link').click(function() {
-        window.links[$('#link_id').val()] = undefined;
-        chrome.storage.local.set({'links': window.links},function() {});
-        window.location.reload();
+      window.links[$('#link_id').val()] = undefined;
+      chrome.storage.local.set({'links': window.links},function() {});
+      window.location.reload();
     });
 
     // set click listener on the close button of the edit popup
     $('#close_link_edit').click(function() {
-        $('#link_edit').hide();
-        window.editMode = false;
+      $('#link_edit').hide();
+      tab.editMode = false;
     });
 
     // set click listener to close the link edit popup
     $(document).keyup(function(e) {
-        if(e.keyCode == 27) {
-            $('#link_edit').hide();
-            window.editMode = false;
-        }
+      if(e.keyCode == 27) {
+        $('#link_edit').hide();
+        tab.editMode = false;
+      }
     });
 
+  },
+
+
+  /**
+   * Calculate dimensions
+   */
+  calcDimensions: function () {
+    var x, prev;
+    for ( var i = 100; i <= 1500; i++ ) {
+      x = window.innerWidth / Math.ceil(window.innerWidth / i);
+      if ( x !== prev ) tab.arr.push(x);
+      prev = x;
+    }
   },
 
 
@@ -138,67 +127,86 @@ var tab = {
    * Fills up the screen with links
    */
   setLinks: function() {
-    var numRows = Math.ceil(window.innerHeight / tab.rowHeight)+1;
-    var numCols = Math.floor(window.innerWidth / tab.colWidth);
+    var numRows = Math.ceil(window.innerHeight / tab.d)+1;
+    var numCols = Math.floor(window.innerWidth / tab.d);
     var numLinks = numRows*numCols;
 
     $('#content').empty();
     for(var counter = 0; counter < numLinks; counter++){
-        var link = window.links[counter] || undefined;
-        if(link == undefined){
-            window.links[counter] = undefined;
-            $('#content').append('<div id="'+counter+'" class="link" data-reveal-id="edit_popup"><div class="cover"></div><img class="filler" src="images/filler.svg" alt="add link image" /></div>');
-        } else if(link['image'] == undefined){
-            var url = 'chrome://favicon/'+link.url;
-            var setLink = function(link) {
-                return function(data) {
-                    if(data.length == 228){
-                        $('#content').append('<div id="'+counter+'" class="link" title="'+link.title+'"><div class="cover"></div><span class="link_title">'+link.title+'</span></div>');
-                    }else{
-                        $('#content').append('<div id="'+counter+'" class="link" title="'+link.title+'"><div class="cover"></div><img src="chrome://favicon/'+link.url+'" alt="link image" /></div>');
-                    }
-                };
-            };
-            $.ajax({url: url, async: false, success: setLink(link)});
-        } else {
-            $('#content').append('<div id="'+counter+'" class="link" title="'+link.title+'"><div class="cover"></div><img src="'+link.image+'" alt="link image" /></div>');
-        }
+      var link = window.links[counter] || undefined;
+      if(link == undefined){
+        window.links[counter] = undefined;
+        $('#content').append('<div id="'+counter+'" class="link" data-reveal-id="edit_popup"><div class="cover"></div><img class="filler" src="images/filler.svg" alt="add link image" /></div>');
+      } else if(link['image'] == undefined){
+        var url = 'chrome://favicon/'+link.url;
+        var setLink = function(link) {
+          return function(data) {
+            if(data.length == 228){
+              $('#content').append('<div id="'+counter+'" class="link" title="'+link.title+'"><div class="cover"></div><span class="link_title">'+link.title+'</span></div>');
+            }else{
+              $('#content').append('<div id="'+counter+'" class="link" title="'+link.title+'"><div class="cover"></div><img src="chrome://favicon/'+link.url+'" alt="link image" /></div>');
+            }
+          };
+        };
+        $.ajax({url: url, async: false, success: setLink(link)});
+      } else {
+        $('#content').append('<div id="'+counter+'" class="link" title="'+link.title+'"><div class="cover"></div><img src="'+link.image+'" alt="link image" /></div>');
+      }
     }
 
-    $('.link').css('width', parseInt(tab.colWidth));
-    $('.link').css('height', parseInt(tab.rowHeight));
-    $('.link img').css('width', parseInt(tab.colWidth/2));
-    $('.link img').css('height', parseInt(tab.rowHeight/2));
-    $('.link img').css('margin-top', parseInt(tab.rowHeight/4));
-    $('.link_title').css('margin-top', parseInt(tab.rowHeight/2.2));
+    $('.link').css('width', parseInt(tab.d));
+    $('.link').css('height', parseInt(tab.d));
+    $('.link img').css('width', parseInt(tab.d/2));
+    $('.link img').css('height', parseInt(tab.d/2));
+    $('.link img').css('margin-top', parseInt(tab.d/4));
+    $('.link_title').css('margin-top', parseInt(tab.d/2.2));
 
     $('.link').click(function(){
-        var link = window.links[$(this).attr('id')];
-        if(link === undefined){
-            // popup menu to select website + logo
-            $('#link_edit h1').text("Add a new link");
-            $('#link_id').val($(this).attr('id'));
-            $('#link_edit').show();
-            $('#link_title').focus();
+      var link = window.links[$(this).attr('id')];
+      if(link === undefined){
+        // popup menu to select website + logo
+        $('#link_edit h1').text("Add a new link");
+        $('#link_id').val($(this).attr('id'));
+        $('#link_edit').show();
+        $('#link_title').focus();
+      } else {
+        if(tab.editMode == true){
+          // popup menu to select a website + logo (website prefilled)
+          $('#link_edit h1').text(link.title);
+          $('#link_title').val(link.title);
+          $('#link_url').val(link.url);
+          $('#link_id').val($(this).attr('id'));
+          $('#link_edit').show();
+          $('#link_title').focus();
         } else {
-            if(window.editMode == true){
-                // popup menu to select a website + logo (website prefilled)
-                $('#link_edit h1').text(link.title);
-                $('#link_title').val(link.title);
-                $('#link_url').val(link.url);
-                $('#link_id').val($(this).attr('id'));
-                $('#link_edit').show();
-                $('#link_title').focus();
-            } else {
-                // actually redirecting the user to the requested page
-                var url = window.links[$(this).attr('id')].url;
-                if(!tab.isValidURL(url)){
-                    url = "http://" + url;
-                }
-                window.location = url;
-            }
+          // actually redirecting the user to the requested page
+          var url = window.links[$(this).attr('id')].url;
+          if(!tab.isValidURL(url)){
+            url = "http://" + url;
+          }
+          window.location = url;
         }
+      }
     });
+  },
+
+
+  /**
+   * Helper function to get the closest number
+   * @param num Number
+   * @param arr Array
+   */
+  closest: function (num, arr) {
+       var curr = arr[0];
+       var diff = Math.abs (num - curr);
+       for (var val = 0; val < arr.length; val++) {
+           var newdiff = Math.abs (num - arr[val]);
+           if (newdiff < diff) {
+               diff = newdiff;
+               curr = arr[val];
+           }
+       }
+       return curr;
   },
 
 
