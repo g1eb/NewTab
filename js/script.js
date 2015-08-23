2568a7e0740b9b61bd149eae18c130d7d3b730b2
Gleb <git@gleb.nl>
2015-08-23 12:29:41 +0200

    Whitespace fix.

--- js/script.js
+++ js/script.js
@@ -226,16 +226,16 @@ var tab = {
    * @param arr Array
    */
   closest: function (num, arr) {
-       var curr = arr[0];
-       var diff = Math.abs (num - curr);
-       for (var val = 0; val < arr.length; val++) {
-           var newdiff = Math.abs (num - arr[val]);
-           if (newdiff < diff) {
-               diff = newdiff;
-               curr = arr[val];
-           }
-       }
-       return curr;
+    var curr = arr[0];
+    var diff = Math.abs (num - curr);
+    for (var val = 0; val < arr.length; val++) {
+      var newdiff = Math.abs (num - arr[val]);
+      if (newdiff < diff) {
+        diff = newdiff;
+        curr = arr[val];
+      }
+    }
+    return curr;
   },
 
 
